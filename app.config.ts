eed4f4d37ff61f97c3036bab7813e35e20f40dc7
Chad $yntax <chad@ttpspodcast.com>
2022-06-04 23:05:48 -0400

    add more helpful error placement for missing rss feed url

--- app.config.ts
+++ app.config.ts
@@ -4,3 +4,8 @@ export const DECENTRALIZED = process.env.DECENTRALIZED === 'true';
 export const BUILD_TS = process.env.BUILD_TS
   ? new Date(Number(process.env.BUILD_TS) * 1000)
   : new Date();
+
+if (!COMING_SOON_ENABLED && !RSS_FEED_URL)
+  throw new Error(
+    `RSS_FEED_URL is required but not present. check your .env file or environment variables`
+  );
