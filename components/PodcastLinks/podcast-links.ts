3a6cafcae5bd316712e8b49aa678b38dffeb755b
Chad $yntax <chad@ttpspodcast.com>
2022-07-08 01:22:52 -0400

    add additional podcast links

--- components/PodcastLinks/podcast-links.ts
+++ components/PodcastLinks/podcast-links.ts
@@ -1,5 +1,9 @@
 import spotifyLogo from './podcast-player-icons/spotify@2x.png';
 import anchorLogo from './podcast-player-icons/anchor-wave@2x.png';
+import amazonMusicLogo from './podcast-player-icons/amazon_music@2x.png';
+import pocketCastsLogo from './podcast-player-icons/pocket_casts@2x.png';
+import radioPublicLogo from './podcast-player-icons/radiopublic@2x.png';
+import stitcherLogo from './podcast-player-icons/stitcher_dark@2x.png';
 import rssLogo from './podcast-player-icons/rss.png';
 
 export const PODCAST_LINKS = [
@@ -9,6 +13,26 @@ export const PODCAST_LINKS = [
     image: spotifyLogo,
   },
   { name: 'Anchor', url: 'https://anchor.fm/ttps', image: anchorLogo },
+  {
+    name: 'Amazon Music',
+    url: 'https://music.amazon.com/podcasts/dca78d9b-8f96-4158-a1f3-0bf677df13e0/talking-tech-while-poking-smot',
+    image: amazonMusicLogo,
+  },
+  {
+    name: 'Pocket Casts',
+    url: 'https://pca.st/069xcy8j',
+    image: pocketCastsLogo,
+  },
+  {
+    name: 'RadioPublic',
+    url: 'https://radiopublic.com/talking-tech-while-poking-smot-G2DQ70',
+    image: radioPublicLogo,
+  },
+  {
+    name: 'Stitcher',
+    url: 'https://www.stitcher.com/podcast/talking-tech-while-poking-smot',
+    image: stitcherLogo,
+  },
   {
     name: 'RSS',
     url: 'https://anchor.fm/s/797bdf30/podcast/rss',
